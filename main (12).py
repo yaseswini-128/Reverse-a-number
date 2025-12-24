8b9de2cc5df5ef3620a7ba107ff70464eb7052e8
yaseswini-128 <yaseswiniphalam@gmail.com>
2025-12-24 15:08:51 +0530

    Add files via upload

---
+++ main (12).py
@@ -0,0 +1,14 @@
+def reverse(n):
+    out=0
+    store=n
+    while n>0:
+        rem=n%10
+        out=out*10+rem
+        n=n//10
+    if store==out:
+       return("palindromic number")  
+    else:
+       return("not a palindromic number")
+    
+n=int(input("Enter the number:"))
+print(reverse(n))
